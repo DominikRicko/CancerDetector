ebacf76d59aefabb626a9aa973c829e72b87db68
Dominik Ričko <dominik.ricko@gmail.com>
2021-02-15 21:34:53 +0100

    Implemented newly added charts to statistics

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ChartDisplayableArray } from '../charts/chartDisplayableArray';
+import { SampleDataContainer } from '../shared/sampleData/SampleData';
+
 @Component({
   selector: 'app-detail',
   templateUrl: './statistics.component.html',
@@ -7,27 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatisticsComponent implements OnInit {
 
-  public data: any[] = [{
-    kind: 'Solar', share: 0.052
-  }, {
-    kind: 'Wind', share: 0.225
-  }, {
-    kind: 'Other', share: 0.192
-  }, {
-    kind: 'Hydroelectric', share: 0.175
-  }, {
-    kind: 'Nuclear', share: 0.238
-  }, {
-    kind: 'Coal', share: 0.118
-  }];
+  readonly ageChartData : ChartDisplayableArray;
+  readonly genderChartData : ChartDisplayableArray;
+  readonly creatinineChartData : ChartDisplayableArray;
+  readonly lyve1ChartData : ChartDisplayableArray;
+  readonly reg1bChartData : ChartDisplayableArray;
+  readonly tff1ChartData : ChartDisplayableArray;
+  readonly reg1aChartData : ChartDisplayableArray;
+  readonly diagnosisChartData : ChartDisplayableArray;
 
+  readonly averagePrecision : number;
+  readonly sampleCount : number;
+  constructor() {
+    this.ageChartData = new ChartDisplayableArray(SampleDataContainer.samples, 'age', 5);
+    this.genderChartData = new ChartDisplayableArray(SampleDataContainer.samples, 'sex', 2, {numeric : false});
+    this.creatinineChartData = new ChartDisplayableArray(SampleDataContainer.samples, 'creatinine', 5);
+    this.lyve1ChartData = new ChartDisplayableArray(SampleDataContainer.samples, 'LYVE1', 5);
+    this.reg1bChartData = new ChartDisplayableArray(SampleDataContainer.samples, 'REG1B',5);
+    this.tff1ChartData = new ChartDisplayableArray(SampleDataContainer.samples, 'TFF1',5);
+    this.reg1aChartData = new ChartDisplayableArray(SampleDataContainer.samples, 'REG1A', 5);
+    this.diagnosisChartData = new ChartDisplayableArray(SampleDataContainer.samples, 'diagnosis', 2, {numeric : false});
 
-  constructor() { }
+    this.sampleCount = SampleDataContainer.samples.length;
+    this.averagePrecision = 0;
+
+    if(this.sampleCount == 0){
+      return;
+    }
+
+    for(const sample of SampleDataContainer.samples){
+      this.averagePrecision+=sample.precision;
+    }
+
+    this.averagePrecision/=this.sampleCount;
+  }
 
   ngOnInit(): void { }
-
-  public labelContent(e: any): string {
-    return e.category;
-  }
 
 }
--- src/app/statistics/statistics.module.ts
+++ src/app/statistics/statistics.module.ts
@@ -7,14 +7,21 @@ import { StatisticsComponent } from './statistics.component';
 import { SharedModule } from '../shared/shared.module';
 
 import { ChartsModule } from '@progress/kendo-angular-charts';
+import { LabelModule } from '@progress/kendo-angular-label';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+
+import { LineChartComponent } from '../charts/line-chart/line-chart.component';
+import { PieChartComponent } from '../charts/pie-chart/pie-chart.component';
 
 @NgModule({
-  declarations: [StatisticsComponent],
+  declarations: [StatisticsComponent, LineChartComponent, PieChartComponent],
   imports: [
     CommonModule,
     SharedModule,
     StatisticsRoutingModule,
-    ChartsModule
+    ChartsModule,
+    LabelModule,
+    LayoutModule
   ]
 })
 export class StatisticsModule {}
